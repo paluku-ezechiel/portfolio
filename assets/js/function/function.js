da23e280b02949e075757391a61b18417f1ed855
palukuezechiel@gmail.com <palukuezechiel@gmail.com>
2025-05-24 16:26:59 +0200

    add footer and function js

---
+++ assets/js/function/function.js
@@ -0,0 +1,11 @@
+var button = document.querySelector(".btn-view");
+
+let listenerFunction = {
+    openProject : () => {
+        
+    }
+}
+
+let setupListener = () => {
+    button.addEventListener("click", listenerFunction.openProject)
+}
